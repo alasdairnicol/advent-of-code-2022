87d762293dc596d2806713e6c0b747765c5e1e3e
Alasdair Nicol <alasdair@thenicols.net>
2022-12-02 10:27:45 +0000

    Add ROCK/PAPER/SCISSORS constants to try to make it clearer

--- day02.py
+++ day02.py
@@ -1,66 +1,88 @@
 #!/usr/bin/env python
 
+# Outcome score
 WIN = 6
 DRAW = 3
 LOSS = 0
 
-shape_scores = {
-    "X": 1,
-    "Y": 2,
-    "Z": 3,
+# Shape scores
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
+
+elf_move_to_shape = {
+    "A": ROCK,
+    "B": PAPER,
+    "C": SCISSORS,
 }
 
-round_1_move_scores = {
-    ("A", "X"): DRAW,
-    ("A", "Y"): WIN,
-    ("A", "Z"): LOSS,
-    ("B", "X"): LOSS,
-    ("B", "Y"): DRAW,
-    ("B", "Z"): WIN,
-    ("C", "X"): WIN,
-    ("C", "Y"): LOSS,
-    ("C", "Z"): DRAW,
+round_1_move_to_shape = {
+    "X": ROCK,
+    "Y": PAPER,
+    "Z": SCISSORS,
 }
 
-round_2_shapes = {
-    ("A", "X"): "Z",
-    ("A", "Y"): "X",
-    ("A", "Z"): "Y",
-    ("B", "X"): "X",
-    ("B", "Y"): "Y",
-    ("B", "Z"): "Z",
-    ("C", "X"): "Y",
-    ("C", "Y"): "Z",
-    ("C", "Z"): "X",
-}
-
-round_2_move_scores = {
+round_2_outcome_scores = {
     "X": LOSS,
     "Y": DRAW,
     "Z": WIN,
 }
 
+score_round = {
+    # (elf_move, your_move): outcome
+    (ROCK, ROCK): DRAW,
+    (ROCK, PAPER): WIN,
+    (ROCK, SCISSORS): LOSS,
+    (PAPER, ROCK): LOSS,
+    (PAPER, PAPER): DRAW,
+    (PAPER, SCISSORS): WIN,
+    (SCISSORS, ROCK): WIN,
+    (SCISSORS, PAPER): LOSS,
+    (SCISSORS, SCISSORS): DRAW,
+}
+
+round_2_shapes = {
+    # (elf_move, outcome): your_move
+    (ROCK, LOSS): SCISSORS,
+    (ROCK, DRAW): ROCK,
+    (ROCK, WIN): PAPER,
+    (PAPER, LOSS): ROCK,
+    (PAPER, DRAW): PAPER,
+    (PAPER, WIN): SCISSORS,
+    (SCISSORS, LOSS): PAPER,
+    (SCISSORS, DRAW): SCISSORS,
+    (SCISSORS, WIN): ROCK,
+}
+
 
 def parse_line(line: str) -> tuple[str, str]:
-    shape_1, shape_2 = line.split(" ", maxsplit=1)
-    return (shape_1, shape_2)
+    pieces = line.split(" ", maxsplit=1)
+    return (pieces[0], pieces[1])
 
 
-def score_round_1(round):
-    return round_1_move_scores[round] + shape_scores[round[1]]
+def score_round_1(round: tuple[str, str]) -> int:
+    elf_shape = elf_move_to_shape[round[0]]
+    your_shape = round_1_move_to_shape[round[1]]
+    outcome_score = score_round[(elf_shape, your_shape)]
+    shape_score = your_shape
+    return outcome_score + shape_score
 
 
-def do_part_1(rounds):
+def do_part_1(rounds: list[tuple[str, str]]) -> int:
     scores = [score_round_1(r) for r in rounds]
     return sum(scores)
 
 
-def score_round_2(round):
-    shape = round_2_shapes[round]
-    return round_2_move_scores[round[1]] + shape_scores[shape]
+def score_round_2(round: tuple[str, str]) -> int:
+    elf_shape = elf_move_to_shape[round[0]]
+    your_outcome = round[1]
+    outcome_score = round_2_outcome_scores[your_outcome]
+    your_shape = round_2_shapes[(elf_shape, outcome_score)]
+    shape_score = your_shape
+    return outcome_score + shape_score
 
 
-def do_part_2(rounds):
+def do_part_2(rounds: list[tuple[str, str]]) -> int:
     scores = [score_round_2(r) for r in rounds]
     return sum(scores)
 
